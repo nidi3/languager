eaf03a911e23d84362b252349095fc3d0dcb3619
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-05-14 00:24:09 +0200

    update pom, add license header

--- languager-core/src/test/java/guru/nidi/languager/KeyExtractorTest.java
+++ languager-core/src/test/java/guru/nidi/languager/KeyExtractorTest.java
@@ -1,15 +1,30 @@
+/*
+ * Copyright (C) 2014 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package guru.nidi.languager;
 
-import org.junit.Test;
 import guru.nidi.languager.crawl.CrawlPattern;
 import guru.nidi.languager.crawl.FindRegexAction;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-import static org.junit.Assert.assertEquals;
 import static guru.nidi.languager.Message.Status.FOUND;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
